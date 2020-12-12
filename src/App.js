7338db473134833829cc02541b93d60ae3e5b6f6
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-13 00:08:28 +0400

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
