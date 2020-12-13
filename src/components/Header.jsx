65771b74cd770dde83a3fc5cf8b0e9a199cb9617
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-13 22:39:49 +0400

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import logo from '../images/logo.svg';
 
 export default function Header() {
   return (
-    <header className='header'/>
+    <header className='header'>
+      <img src={logo} alt="" className="header__logo"/>
+    </header>
   )
 }
