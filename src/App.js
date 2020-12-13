fb2e73f05ec04f878e2bc85b5788bcfeac209b6d
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-13 22:15:22 +0400

    12

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import './App.css';
 import React from "react";
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className='header'/>
-      <nav className='nav'>
-        <ul>
-          <li><a href="#s">Profile</a></li>
-          <li><a href="#s">Messages</a></li>
-          <li><a href="#s">News</a></li>
-          <li><a href="#s">Music</a></li>
-          <li><a href="#s">Settings</a></li>
-        </ul>
-      </nav>
-      <main className='content'>
-        <img
-          src="https://1.bp.blogspot.com/-m4Kh7YKtojo/Xom1Qfqh1dI/AAAAAAAAGbA/l6l3NT_pOBAbdmmUhvRqRptzeycSmHl4ACEwYBhgLKs0DAMBZVoBxUmBbbZsX2B-IEqLYD9G_8peDtCobuQGeXVvlJRjNNagHAwqBRd-j6gxWiDATYohFUlQ8-nnW6oGWbsLJ7iNkgu77lo-wqfk4MmawQqtohjhKIwj9TB5k7YZ59KaAyIMXl88QhuxTtM9Vxt8jJMPWr3cBP7CysrxwbkrQpBSJEpdk2Ui9t01O6Od3DKmcA1safs55GJbWEaYj8-4ZQGqttknmbM-OX8qru4g4LqV1oA20uTDievlJlzmled7X0VlM1TKtXRXDXpHeoDypsVGmlGyxn7up_6QtigK_uSFxUxes2tXKG7eE4-qZ-F9IxCH0Cn3eJUmzH2ptgJL_h1DnHq6bVMyE26mIWSueex2UtR0SzhwLEU7mkwo2nIpsB-h8DijWKAqZHKScGoLWQwNCsjUIHg_eCuOAoJQSiygxMZmJpYkFPHMp6WdatOzxSVjmaK_p9f43oNY1qwHPjgSebojaGnCSZVkt0AvNgWtChZEKvsZqjbPyoU30BGfUa-dMCOLE3OsdiDLUibKv2GYRwfqyYx_Kkwy6ybT-q6flgIkpj8zCWKBtk3TE66LzAVwEwQh-f-1OmrszR0GYVzyu0PyLKgUAkkwwvO-m9AU/s640/Grid%2BUltimate%2BEdition%2BPC%2BGame%2BFree%2BDownload%2BFull%2BVersion%2BCompressed%2B31.6GB%2B6.jpg"
-          alt="машина"/>
-        <div>
-          ava+description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>Post1</div>
-            <div>Post2</div>
-          </div>
-        </div>
-      </main>
+      <Header/>
+      <Navbar/>
+      <Profile/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li><a href="#s">Profile</a></li>
+        <li><a href="#s">Messages</a></li>
+        <li><a href="#s">News</a></li>
+        <li><a href="#s">Music</a></li>
+        <li><a href="#s">Settings</a></li>
+      </ul>
+    </nav>
+  )
+}
