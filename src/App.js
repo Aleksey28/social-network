2dadca3946c8d4d38c5e75ef5c4b5a83df108897
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-05 17:03:28 +0400

    added base markup for dialogs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Route, Switch, HashRouter, Redirect } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
@@ -12,19 +12,17 @@ function App() {
       <Header/>
       <Navbar/>
       <div className="app-wrapper__content">
-        <HashRouter>
-          <Switch>
-            <Route path="/profile">
-              <Profile/>
-            </Route>
-            <Route path="/messages">
-              <Messages/>
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/profile"/>
-            </Route>
-          </Switch>
-        </HashRouter>
+        <Switch>
+          <Route path="/profile">
+            <Profile/>
+          </Route>
+          <Route path="/messages">
+            <Messages/>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/profile"/>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
