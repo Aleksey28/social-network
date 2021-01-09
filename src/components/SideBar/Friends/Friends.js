f6adf7ad19012a45f7484611adbdbbee106527ad
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-09 21:18:58 +0400

    added key for Friends.js

--- src/components/SideBar/Friends/Friends.js
+++ src/components/SideBar/Friends/Friends.js
@@ -5,7 +5,7 @@ import FriendMini from "./FriendMini/FriendMini";
 const Friends = ({ friends }) => {
   return (
       <ul className={classes.list}>
-        {friends.map((item) => <FriendMini {...item}/>)}
+        {friends.map((item) => <FriendMini key={item.id} {...item}/>)}
       </ul>
   );
 };
