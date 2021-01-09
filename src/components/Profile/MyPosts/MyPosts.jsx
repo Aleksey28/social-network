83cbed95dbab2cd0d2ac98b2a2c66591cf2370dd
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-09 22:47:36 +0400

    added handler for button new post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import Post from "./Post/Post";
 
 const MyPosts = ({ profilePage }) => {
 
+  const newPostElement = useRef();
+
   const postsElements = profilePage.postsData.map((item) => <Post key={item.id} message={item.message}/>);
+
+  const handleClickOnButton = () => {
+    alert(newPostElement.current.value);
+  };
 
   return (
     <div>
       My posts
       <div>
-        <textarea placeholder={"New post"}/>
-        <button>Add post</button>
+        <textarea ref={newPostElement} placeholder={"New post"}/>
+        <button onClick={handleClickOnButton}>Add post</button>
       </div>
       <div>
         {postsElements}
