8fe5493c72098ad4404772a9d80dcd2e3a7ef442
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-11 22:40:54 +0300

    feat: changed Users from function component to class

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 import classes from "./User.module.css";
 import axios from "axios";
-import emptyAvatar from '../../images/empty_avatar.svg';
+import emptyAvatar from "../../images/empty_avatar.svg";
 
-const Users = ({ users, setUsers, follow, unfollow }) => {
-
-  if (users.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => setUsers(response.data.items))
-      .catch(console.log);
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getUsers = this.getUsers.bind(this);
   }
 
-  return (
-    <ul>
-      {
-        users.map(u => (
-          <li>
-            <div>
-              <img src={u.photos.small || emptyAvatar} alt="avatar" className={classes.avatar}/>
-              {u.followed
-               ? <button onClick={() => {unfollow(u.id);}}>Follow</button>
-               : <button onClick={() => {follow(u.id);}}>Unfollow</button>}
-            </div>
-            <div>
+  getUsers() {
+    if (this.props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => this.props.setUsers(response.data.items))
+        .catch(console.log);
+    }
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.props.users.map(u => (
+            <li>
               <div>
-                <p>{u.name}</p>
-                <p>{u.status}</p>
+                <img src={u.photos.small || emptyAvatar} alt="avatar" className={classes.avatar}/>
+                {u.followed
+                 ? <button onClick={() => {this.props.unfollow(u.id);}}>Follow</button>
+                 : <button onClick={() => {this.props.follow(u.id);}}>Unfollow</button>}
               </div>
-            </div>
-          </li>
-        ))
-      }
-    </ul>
-  );
-};
+              <div>
+                <div>
+                  <p>{u.name}</p>
+                  <p>{u.status}</p>
+                </div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
+}
 
 export default Users;
