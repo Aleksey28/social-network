32dbde7a3bb00d0750e173bdc06e6c127337b0b1
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-17 20:53:21 +0300

    feat: added list of page output

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import emptyAvatar from "../../images/empty_avatar.svg";
 
 class Users extends React.Component {
+
   componentDidMount() {
     axios.get("https://social-network.samuraijs.com/api/1.0/users")
       .then(response => this.props.setUsers(response.data.items))
@@ -11,27 +12,44 @@ class Users extends React.Component {
   }
 
   render() {
+
+    const countPages = this.props.usersCount / this.props.pageSize;
+    const pagesBar = [];
+
+    for (let i = 0; i < countPages; i++) {
+      pagesBar.push(
+        <li>{i + 1}</li>,
+      );
+    }
+
     return (
-      <ul>
-        {
-          this.props.users.map(u => (
-            <li>
-              <div>
-                <img src={u.photos.small || emptyAvatar} alt="avatar" className={classes.avatar}/>
-                {u.followed
-                 ? <button onClick={() => {this.props.unfollow(u.id);}}>Follow</button>
-                 : <button onClick={() => {this.props.follow(u.id);}}>Unfollow</button>}
-              </div>
-              <div>
+      <div>
+        <nav>
+          <ul>
+            {pagesBar}
+          </ul>
+        </nav>
+        <ul>
+          {
+            this.props.users.map(u => (
+              <li>
                 <div>
-                  <p>{u.name}</p>
-                  <p>{u.status}</p>
+                  <img src={u.photos.small || emptyAvatar} alt="avatar" className={classes.avatar}/>
+                  {u.followed
+                   ? <button onClick={() => {this.props.unfollow(u.id);}}>Follow</button>
+                   : <button onClick={() => {this.props.follow(u.id);}}>Unfollow</button>}
                 </div>
-              </div>
-            </li>
-          ))
-        }
-      </ul>
+                <div>
+                  <div>
+                    <p>{u.name}</p>
+                    <p>{u.status}</p>
+                  </div>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     );
   }
 }
