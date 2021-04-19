bd420d08c987c9556aa261b555521f9bee6307d0
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-19 22:07:44 +0300

    style: formatted code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from '../../images/logo.svg';
-import classes from './Header.module.css';
+import React from "react";
+import logo from "../../images/logo.svg";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={classes.header}>
       <img src={logo} alt="" className={classes.logo}/>
     </header>
-  )
+  );
 }
