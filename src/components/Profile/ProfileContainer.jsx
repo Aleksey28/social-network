ccc6d5b362614442c63fd09f56cc7c6f1e25f1df
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-26 22:34:06 +0300

    feat: added getting profile info from API.

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import { setUserInfo } from "../../redux/profileReducer";
+import axios from "axios";
+import { apiSamuraiSettings } from "../../utils/constants";
 
 class ProfileContainer extends React.Component {
+  componentDidMount() {
+    axios.get(`${apiSamuraiSettings.baseUrl}/profile/2`)
+      .then(res => this.props.setUserInfo(res.data))
+      .catch(console.log);
+  }
+
   render() {
     return (
       <Profile {...this.props} />
