bfb1c6102ee44025fea134fd94682931430e9ec7
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-29 20:32:09 +0300

    feat: created authReducer.js

---
+++ src/redux/authReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+
+};
+
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default profileReducer;
+
+export {
+
+};
