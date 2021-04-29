5c150d220baec6f023aecdc75ec3200b1d6898fb
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-29 20:41:55 +0300

    feat: added actions SET_USER_DATA

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,19 +1,32 @@
+const SET_USER_DATA = "SET_USER_DATA";
+
 const initialState = {
-  email: "",
+  email: '',
   login: '',
   userId: '',
   isAuth: false
 };
 
-const profileReducer = (state = initialState, action) => {
+const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_USER_DATA:
+      return  {
+        ...state,
+        ...action.data,
+        isAuth: true
+      }
     default:
       return state;
   }
 };
 
-export default profileReducer;
+const setUserData = ({email, login, userId}) => ({
+  type: SET_USER_DATA,
+  data: {email, login, userId}
+})
+
+export default authReducer;
 
 export {
-
+  setUserData
 };
