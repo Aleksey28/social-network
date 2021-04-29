a753d15ae4707366351044d1d431ee76c1138856
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-04-29 22:51:24 +0300

    feat: created HeaderContainer

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import Header from './Header';
 import { setUserData } from '../../redux/authReducer';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { apiSamuraiSettings } from '../../utils/constants';
 
 class HeaderContainer extends React.Component {
+
+  componentDidMount() {
+    axios.get( `${ apiSamuraiSettings.baseUrl }/auth/me`, { withCredentials: true } )
+      .then( res => {
+        if ( res.data.resultCode === 1 ) {
+          throw new Error( res.data.messages[0] );
+        }
+        const { email, login, id: userId } = res.data.data;
+        this.props.setUserData( { email, login, userId } );
+      } )
+      .catch( console.log );
+  }
+
   render() {
     return (
-      <Header/>
+      <Header { ...this.props }/>
     );
   }
 }
