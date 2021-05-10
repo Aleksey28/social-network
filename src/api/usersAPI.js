c75dfa7e30d132412ac6bac53daa6dda3a79bdf9
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-10 14:17:51 +0300

    feat: replaced axios with instance

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,31 +1,30 @@
 import axios from 'axios';
-import { apiSamuraiSettings } from '../utils/constants';
 
-export default class usersAPI {
-  // constructor( props ) {
-  //   const instance =
-  //
-  // }
+class usersAPI {
+  constructor() {
+    this._instance = axios.create( {
+      baseURL: 'https://social-network.samuraijs.com/api/1.0',
+      withCredentials: true,
+      headers: {
+        'API-KEY': '9b281f13-b744-4cc8-8a50-53b0d0396ab3',
+      },
+    } );
+  }
+
   getUsers( page, pageSize ) {
-    return axios.get(
-      `${ apiSamuraiSettings.baseUrl }/users?count=${ pageSize }&page=${ page + 1 }`,
-      { withCredentials: true },
-    );
+    return this._instance.get( `/users?count=${ pageSize }&page=${ page }` )
+      .then(response => response.data);
   }
 
-  follow(id) {
-    return axios.post( `${ apiSamuraiSettings.baseUrl }/follow/${ id }`, {}, {
-      withCredentials: true, headers: {
-        'API-KEY': apiSamuraiSettings.token,
-      },
-    } );
+  follow( id ) {
+    return this._instance.post( `/follow/${ id }`, {} )
+      .then(response => response.data);
   }
 
-  unfollow(id) {
-    return axios.delete( `${ apiSamuraiSettings.baseUrl }/follow/${ id }`, {
-      withCredentials: true, headers: {
-        'API-KEY': apiSamuraiSettings.token,
-      },
-    } );
+  unfollow( id ) {
+    return this._instance.delete( `/follow/${ id }` )
+      .then(response => response.data);
   }
 }
+
+export default new usersAPI();
