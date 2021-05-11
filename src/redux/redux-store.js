e5b96bb9d2438d54a372ef7d773407a522543022
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-11 23:56:15 +0300

    feat: connected thunk

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,10 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import friendsReducer from "./friendsReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
+import thunkMiddleware from 'redux-thunk';
 
 const reducers = combineReducers({
   profilePage: profileReducer,
@@ -13,6 +14,6 @@ const reducers = combineReducers({
   auth: authReducer,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
