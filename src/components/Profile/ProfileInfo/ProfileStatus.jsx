075bd37a27dbaa16d5c8844f435b354edbf1afb2
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-16 21:35:16 +0300

    feat: added state editMode

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class ProfileStatus extends React.Component {
+  state = {
+    editMode: false,
+  };
 
+  render() {
+    return (
+      <div>
+        <span>{ this.props.status }</span>
+        <input>{ this.props.status }</input>
+      </div>
+    );
+  }
 }
 
 export default ProfileStatus;
