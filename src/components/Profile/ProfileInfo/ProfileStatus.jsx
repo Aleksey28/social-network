2c557b9f50a0acc54928185c861b1d41db45a9f2
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-16 21:46:44 +0300

    feat: added activate and deactivate edit mode in UI

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 
 class ProfileStatus extends React.Component {
-  state = {
-    editMode: false,
-  };
+
+  constructor( props ) {
+    super( props );
+    this.activateEditMode = this.activateEditMode.bind( this );
+    this.deactivateEditMode = this.deactivateEditMode.bind( this );
+    this.state = {
+      editMode: false,
+    };
+  }
 
   activateEditMode() {
-    this.setState({
-      editMode: true
-    })
+    this.setState( {
+      editMode: true,
+    } );
   }
 
   deactivateEditMode() {
-    this.setState({
-      editMode: true
-    })
+    this.setState( {
+      editMode: false,
+    } );
   }
 
   render() {
     return (
       <div>
         { !this.state.editMode
-          ? <span>{ this.props.status }</span>
-          : <input value={ this.props.status }/> }
+          ? <span onClick={ this.activateEditMode }>{ this.props.status }</span>
+          : <input value={ this.props.status } onBlur={ this.deactivateEditMode } autoFocus={ true }/> }
       </div>
     );
   }
