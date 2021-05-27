aa43b2223f3c438ec59f5cfc6bb774b66849f065
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-27 23:53:09 +0300

    feat: added fields in login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,33 @@
-function LoginForm() {
-  return (
-    <form>
+import { Field, reduxForm } from 'redux-form';
+import React from 'react';
 
+function LoginForm( { handleSubmit } ) {
+  return (
+    <form onSubmit={ handleSubmit }>
+      <Field name="login" placeholder="Login" component="input"/>
+      <Field name="password" placeholder="Password" component="input"/>
+      <div>
+        <Field name="rememberMe" component="input" type="checkbox"/>remember me
+      </div>
+      <button type="submit">Login</button>
     </form>
   );
 }
 
+const LoginReduxForm = reduxForm( {
+  form: 'login',
+} )( LoginForm );
+
 function Login() {
+
+  const handleSubmit = ( formData ) => {
+    console.log( formData );
+  };
+
   return (
     <div>
       <h1>LOGIN</h1>
-      <LoginForm/>
+      <LoginReduxForm onSubmit={ handleSubmit }/>
     </div>
   );
 }
