34ca8edadd09e95bdd7b1052869379abb582d4ed
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-05-27 23:34:53 +0300

    feat: added component LoginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,18 @@
-function Login () {
+function LoginForm() {
   return (
-    <h1>LOGIN</h1>
-  )
+    <form>
+
+    </form>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <LoginForm/>
+    </div>
+  );
 }
 
 export default Login;
