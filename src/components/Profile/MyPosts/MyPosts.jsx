773c8d40f95ed380fa0bd7ab062d5f946afacebf
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-06-05 23:53:48 +0300

    feat: connected Textarea to field in MyPosts and add validation

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Post from './Post/Post';
 import { Field, reduxForm } from 'redux-form';
+import { Textarea } from '../../common/FormsControls/FormsControls';
+import { maxLength30, required } from '../../../utils/validators';
 
 const MyPostsForm = ( { handleSubmit } ) => {
   return (
     <form onSubmit={ handleSubmit }>
-      <Field name="newPost" placeholder="New post" component="textarea"/>
+      <Field name="newPost"
+             placeholder="New post"
+             component={ Textarea }
+             validate={ [required, maxLength30] }/>
       <button type="submit">Add post</button>
     </form>
   );
@@ -18,7 +23,6 @@ const MyPostsReduxForm = reduxForm( {
 const MyPosts = ( { addPost, profilePage } ) => {
 
   const postsElements = profilePage.postsData.map( ( item ) => <Post key={ item.id } message={ item.message }/> );
-
   const handleAddPost = ( formData ) => {
     addPost( formData );
   };
