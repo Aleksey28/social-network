c14436ad807ec9a2417bcced03d53be8ef6bbb34
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-06-05 23:58:41 +0300

    feat: connected validate for field

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,13 @@
 import { Field, reduxForm } from 'redux-form';
 import React from 'react';
 import { Input } from '../common/FormsControls/FormsControls';
+import { maxLength30, required } from '../../utils/validators';
 
 function LoginForm( { handleSubmit } ) {
   return (
     <form onSubmit={ handleSubmit }>
-      <Field name="email" placeholder="Email" component={ Input }/>
-      <Field name="password" placeholder="Password" component={ Input }/>
+      <Field name="email" placeholder="Email" component={ Input } validate={ [required, maxLength30] }/>
+      <Field name="password" placeholder="Password" component={ Input } validate={ [required, maxLength30] }/>
       <div>
         <Field name="rememberMe" component={ Input } type="checkbox"/>remember me
       </div>
