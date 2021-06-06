f5dfda328f49b984d9a149b084a0b99cc97973f6
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-06-06 15:09:32 +0300

    feat: added logout in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,19 @@ import logo from '../../images/logo.svg';
 import classes from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 
-export default function Header( { login, isAuth } ) {
+export default function Header( { login, logout, isAuth } ) {
   return (
     <header className={ classes.header }>
-      <img src={ logo } alt="" className={ classes.logo }/>
+      <img src={ logo } alt="" className={ classes.header__logo }/>
       <div>
         { isAuth
-          ? <p>{ login }</p>
-          : <NavLink to='/auth'>Login</NavLink> }
+          ? (
+            <div className={ classes.header__nav }>
+              <p>{ login }</p>
+              <button onClick={ logout }>Log out</button>
+            </div>
+          )
+          : <NavLink to="/auth">Login</NavLink> }
       </div>
     </header>
   );
