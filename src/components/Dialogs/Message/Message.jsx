90dd053aceb5591ad0ec8ab17c5ff4e0401cdabc
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-06-12 19:45:24 +0300

    feat: changed conditions for classes

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import classes from "./Message.module.css";
+import React from 'react';
+import classes from './Message.module.css';
 
-const Message = ({ message, owner }) => {
+const Message = ( { message, owner } ) => {
 
   return (
-    <div className={`${classes.message} ${owner.id === 1 && classes.message_own}`}>
-      <img src={owner.avatar} alt="respondent's avatar"/>
-      {message}
+    <div className={ `${ classes.message } ${ owner.id === 1 ? classes.message_own : undefined }` }>
+      <img src={ owner.avatar } alt="respondent's avatar"/>
+      { message }
     </div>
   );
 };
