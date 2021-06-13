81b9da1a9dd6c2d6d7760e52a2d013516f7721d9
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-06-13 20:37:38 +0300

    feat: added selector for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { initializing } from '../../redux/app/reducer';
 import Preloader from '../common/Preloader/Preloader';
+import { getInitializedState } from '../../redux/app/selector';
 
 class App extends React.Component {
   componentDidMount() {
@@ -57,7 +58,7 @@ class App extends React.Component {
 
 const mapStateToProps = ( state ) => ({
   isAuth: state.auth.isAuth,
-  initialized: state.app.initialized,
+  initialized: getInitializedState( state ),
 });
 
 const mapDispatchToProps = {
---
+++ src/redux/app/selector.js
@@ -0,0 +1,3 @@
+export const getInitializedState = ( state ) => {
+  return state.app.initialized;
+};
