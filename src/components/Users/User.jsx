506a60eaeb4da9486ffbdb6b931c17cfc83c8f58
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-08 00:13:47 +0300

    refactor: added User

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+import emptyAvatar from '../../images/empty_avatar.svg';
+import classes from './User.module.css';
+import React from 'react';
+
+const User = ( { id, name, status, photos, followed, isTogglingFollowUsers, follow, unfollow } ) => {
+  return (
+    <div>
+      <div>
+        <NavLink to={ `/profile/${ id }` }>
+          <img src={ photos.small || emptyAvatar } alt="avatar" className={ classes.avatar }/>
+        </NavLink>
+        { !followed
+          ? <button disabled={ isTogglingFollowUsers.some( id => id === id ) }
+                    onClick={ () => {follow( id );} }>Follow</button>
+          : <button disabled={ isTogglingFollowUsers.some( id => id === id ) }
+                    onClick={ () => {unfollow( id );} }>Unfollow</button> }
+      </div>
+      <div>
+        <div>
+          <p>{ name }</p>
+          <p>{ status }</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import classes from './User.module.css';
-import emptyAvatar from '../../images/empty_avatar.svg';
 import Preloader from '../common/Preloader/Preloader';
-import { NavLink } from 'react-router-dom';
 import Paginator from '../common/Paginator/Paginator';
+import User from './User';
 
 function Users( {
   users,
@@ -31,25 +29,11 @@ function Users( {
       <Paginator currentPage={ currentPage } countPages={ countPages } onClick={ handleClickOnPage }/>
       <ul>
         {
-          users.map( ( { id, name, status, photos, followed } ) => (
-            <li key={ id }>
-              <div>
-                <NavLink to={ `/profile/${ id }` }>
-                  <img src={ photos.small || emptyAvatar } alt="avatar" className={ classes.avatar }/>
-                </NavLink>
-                { !followed
-                  ? <button disabled={ isTogglingFollowUsers.some( id => id === id ) }
-                            onClick={ () => {follow( id );} }>Follow</button>
-                  : <button disabled={ isTogglingFollowUsers.some( id => id === id ) }
-                            onClick={ () => {unfollow( id );} }>Unfollow</button> }
-              </div>
-              <div>
-                <div>
-                  <p>{ name }</p>
-                  <p>{ status }</p>
-                </div>
-              </div>
-            </li>
+          users.map( ( userData ) => (
+            <User { ...userData }
+                  isTogglingFollowUsers={ isTogglingFollowUsers }
+                  follow={ follow }
+                  unfollow={ unfollow }/>
           ) )
         }
       </ul>
