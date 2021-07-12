95a012829e83c885d20e32f4dd530c1c23191f75
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-12 23:21:46 +0300

    feat: added support for redux devtools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import profileReducer from './profile/reducer';
 import dialogsReducer from './dialogs/reducer';
 import friendsReducer from './friends/reducer';
@@ -18,6 +18,9 @@ const reducers = combineReducers( {
   app: appReducer,
 } );
 
-const store = createStore( reducers, applyMiddleware( thunkMiddleware ) );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore( reducers, composeEnhancers( applyMiddleware( thunkMiddleware ) ) );
+
+window.__store__ = store;
 
 export default store;
