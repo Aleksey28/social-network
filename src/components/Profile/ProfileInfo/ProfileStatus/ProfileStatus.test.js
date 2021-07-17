28945783741294860849def74311b905989764e8
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-18 01:46:00 +0300

    test: added test Should call callback updateStatus

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.js
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.js
@@ -27,4 +27,15 @@ describe( 'Profile status', () => {
 
     expect( queryByDisplayValue( status ) ).not.toBeNull();
   } );
+
+  it( 'Should call callback updateStatus', () => {
+    const status = 'I\'m the best';
+    const mock = jest.fn();
+    const { container, queryByText } = render( <ProfileStatus status={ status } updateUserStatus={ mock }/> );
+
+    userEvent.click( queryByText( status, { selector: 'span' } ) );
+    userEvent.click( container );
+
+    expect( mock.mock.calls.length ).toEqual( 1 );
+  } );
 } );
