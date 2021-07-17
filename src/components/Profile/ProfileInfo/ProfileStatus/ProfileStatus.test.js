824d51fb556c1ef93b6073fdc49b33a78d7073a4
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-18 00:56:22 +0300

    test: added test Should display <input> after creation with correct status in editMode

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.js
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.js
@@ -1,6 +1,7 @@
 import ProfileStatus from './ProfileStatus';
 import { cleanup, render } from '@testing-library/react';
 import { expect } from 'chai';
+import userEvent from '@testing-library/user-event';
 
 describe( 'Profile status', () => {
   afterEach( cleanup );
@@ -12,10 +13,19 @@ describe( 'Profile status', () => {
     expect( queryByText( status, { selector: 'span' } ) ).not.null;
   } );
 
-  it('Should display <input> after creation with status', () => {
+  it( 'Should display <input> after creation with status', () => {
     const status = 'I\'m the best';
     const { queryByText } = render( <ProfileStatus status={ status }/> );
 
     expect( queryByText( status, { selector: 'input' } ) ).to.null;
-  })
+  } );
+
+  it( 'Should display <input> after creation with correct status in editMode', () => {
+    const status = 'I\'m the best';
+    const { queryByText, queryByDisplayValue } = render( <ProfileStatus status={ status }/> );
+
+    userEvent.click( queryByText( status, { selector: 'span' } ) );
+
+    expect( queryByDisplayValue( status ) ).not.null;
+  } );
 } );
