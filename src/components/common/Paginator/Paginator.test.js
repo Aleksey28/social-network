a86d40b4d93f6d864ecc88718fc2b1929de3148e
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-20 23:46:59 +0300

    test: fixed props

--- src/components/common/Paginator/Paginator.test.js
+++ src/components/common/Paginator/Paginator.test.js
@@ -7,7 +7,7 @@ describe( 'Paginator', () => {
   it( 'Pages count is 11 but should be showed only 10', () => {
     const { queryAllByText } = render( <Paginator totalItemsCount={ 11 }
                                                   currentItem={ 1 }
-                                                  itemsProtionSize={ 10 }
+                                                  itemsPortionSize={ 10 }
                                                   onClick={ () => {} }/> );
     expect( queryAllByText( /\d+/ ).length ).toEqual( 10 );
   } );
@@ -15,7 +15,7 @@ describe( 'Paginator', () => {
   it( 'Is pages count is more then 10 button next should be present', () => {
     const { queryByText } = render( <Paginator totalItemsCount={ 11 }
                                                currentItem={ 1 }
-                                               itemsProtionSize={ 10 }
+                                               itemsPortionSize={ 10 }
                                                onClick={ () => {} }/> );
 
     expect( queryByText( /next/i ) ).not.toBeNull();
