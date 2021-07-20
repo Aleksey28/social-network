044dcfca5e38a26242486e4d57533e7c4e5e2adf
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-20 23:49:17 +0300

    refactor: renamed props

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,14 +1,14 @@
 import classes from './Paginator.module.css';
 import React from 'react';
 
-const Paginator = ( { currentPage, countPages, onClick } ) => {
+const Paginator = ( { totalItemsCount, currentItem, onClick } ) => {
   const pagesBar = [];
 
-  for ( let i = 0; i < countPages; i++ ) {
+  for ( let i = 0; i < totalItemsCount; i++ ) {
     pagesBar.push(
       <li
         key={ i }
-        className={ `${ classes.pages__item } ${ currentPage === i
+        className={ `${ classes.pages__item } ${ currentItem === i
                                                  ? classes.pages__item_selected
                                                  : undefined }` }
         onClick={ () => onClick( i ) }>
