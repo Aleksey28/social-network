756b8b17df62245bfaf460239b91c2559143469c
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-20 23:56:21 +0300

    feat: added calculating potions

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,8 +1,12 @@
 import classes from './Paginator.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-const Paginator = ( { totalItemsCount, currentItem, onClick } ) => {
+const Paginator = ( { totalItemsCount, currentItem, onClick, itemsPortionSize = 10 } ) => {
+  const [currentPortion, setCurrentPortion] = useState( 1 );
   const pagesBar = [];
+  const totalCountPortion = Math.ceil( totalItemsCount / itemsPortionSize );
+  const leftNumberItemOfPortion = itemsPortionSize * (currentPortion - 1) + 1;
+  const rightNumberItemOfPortion = Math.min( itemsPortionSize * (currentPortion), totalItemsCount );
 
   for ( let i = 0; i < totalItemsCount; i++ ) {
     pagesBar.push(
