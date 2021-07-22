f0ebc18a92bff712fbfc9a9cca803b2546fa3c36
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-07-22 23:36:39 +0300

    feat: added calculating currentPortion

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -2,11 +2,11 @@ import classes from './Paginator.module.css';
 import React, { useState } from 'react';
 
 const Paginator = ( { totalItemsCount, currentItem, onClick, itemsPortionSize = 10 } ) => {
-  const [currentPortion, setCurrentPortion] = useState( 1 );
+  const [currentPortion, setCurrentPortion] = useState( Math.ceil( (currentItem + 1) / itemsPortionSize ) );
   const pagesBar = [];
   const totalCountPortion = Math.ceil( totalItemsCount / itemsPortionSize );
   const leftNumberItemOfPortion = itemsPortionSize * (currentPortion - 1);
-  const rightNumberItemOfPortion = Math.min( itemsPortionSize * (currentPortion), totalItemsCount ) - 1;
+  const rightNumberItemOfPortion = Math.min( itemsPortionSize * currentPortion, totalItemsCount ) - 1;
 
   const handleClickPrev = () => {
     setCurrentPortion( (prevState => prevState - 1) );
