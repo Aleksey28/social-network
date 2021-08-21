1eabbc800a0d1a5fd19db96257b0f6aeda59f4bb
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-21 22:03:16 +0300

    refactor: added type for profile selector

---
+++ src/redux/profile/selector.ts
@@ -0,0 +1,12 @@
+export const getUserInfoState = ( state: any ) => {
+  return state.profilePage.userInfo;
+};
+export const getUserStatusState = ( state: any ) => {
+  return state.profilePage.userStatus;
+};
+export const getUserIdState = ( state: any ) => {
+  return state.profilePage.userId;
+};
+export const getPostsData = ( state: any ) => {
+  return state.profilePage.postsData;
+};
