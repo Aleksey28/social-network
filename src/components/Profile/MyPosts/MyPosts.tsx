b313ec18ae6b9ff72cf0e87f85f167ae754836eb
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-26 23:56:28 +0300

    type: added type for MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { FormEventHandler } from 'react';
 import Post from './Post/Post';
 import { Field, reduxForm } from 'redux-form';
 import { Textarea } from '../../common/FormsControls/FormsControls';
 import { maxLength30, required } from '../../../utils/validators';
+import { InitialState } from '../../../redux/profile/reducer';
 
-const MyPostsForm = ( { handleSubmit } ) => {
+interface MyPostsFormProps {
+  handleSubmit: FormEventHandler<HTMLFormElement>;
+}
+
+interface MyPostsProps {
+  addPost: any;
+  postsData: InitialState['postsData'];
+}
+
+interface FormData {
+  newPost: string;
+}
+
+const MyPostsForm = ( { handleSubmit }: MyPostsFormProps ): JSX.Element => {
   return (
     <form onSubmit={ handleSubmit }>
       <Field name="newPost"
@@ -20,16 +34,17 @@ const MyPostsReduxForm = reduxForm( {
   form: 'newPost',
 } )( MyPostsForm );
 
-const MyPosts = React.memo( ( { addPost, postsData } ) => {
+const MyPosts = React.memo( ( { addPost, postsData }: MyPostsProps ): JSX.Element => {
 
   const postsElements = postsData.map( ( {id, message} ) => <Post key={ id } message={ message }/> );
-  const handleAddPost = ( formData ) => {
+  const handleAddPost = ( formData: FormData ) => {
     addPost( formData );
   };
 
   return (
     <div>
       My posts
+      {/*@ts-ignore*/}
       <MyPostsReduxForm onSubmit={ handleAddPost }/>
       <div>
         { postsElements }
