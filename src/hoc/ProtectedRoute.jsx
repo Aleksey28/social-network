debe1ab7954f5ce2b9d46661b133ae3b944f299c
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-28 23:53:13 +0300

    type: added types for ProtectedRoute

--- src/hoc/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Redirect, Route } from 'react-router';
-
-function ProtectedRoute( { path, render, condition, to } ) {
-  return condition
-         ? <Route path={ path } render={ render }/>
-         : <Redirect to={ to }/>;
-}
-
-export default ProtectedRoute;
