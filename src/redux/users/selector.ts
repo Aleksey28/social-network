1385c028928ac418536d2858b769eaf888b05262
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-30 23:26:01 +0300

    type: fixed types in users selector

--- src/redux/users/selector.ts
+++ src/redux/users/selector.ts
@@ -1,6 +1,8 @@
-export const getUsersState = ( state: any ) => state.usersPage.users;
-export const getUsersCountState = ( state: any ) => state.usersPage.usersCount;
-export const getPageSizeState = ( state: any ) => state.usersPage.pageSize;
-export const getCurrentPageState = ( state: any ) => state.usersPage.currentPage;
-export const getIsFetchingState = ( state: any ) => state.usersPage.isFetching;
-export const getIsTogglingFollowUsersState = ( state: any ) => state.usersPage.isTogglingFollowUsers;
+import { AppStateType } from '../redux-store';
+
+export const getUsersState                 = (state: AppStateType) => state.usersPage.users;
+export const getUsersCountState            = (state: AppStateType) => state.usersPage.usersCount;
+export const getPageSizeState              = (state: AppStateType) => state.usersPage.pageSize;
+export const getCurrentPageState           = (state: AppStateType) => state.usersPage.currentPage;
+export const getIsFetchingState            = (state: AppStateType) => state.usersPage.isFetching;
+export const getIsTogglingFollowUsersState = (state: AppStateType) => state.usersPage.isTogglingFollowUsers;
