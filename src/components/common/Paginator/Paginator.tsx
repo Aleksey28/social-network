9db01b15d4a6b0fa198d15e9ca8a744dc4c6aab5
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-30 23:02:47 +0300

    type: fixed types in Paginator.tsx

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -8,43 +8,43 @@ interface Paginator {
   itemsPortionSize?: number;
 }
 
-const Paginator = ( { totalItemsCount, currentItem, onClick, itemsPortionSize = 10 }: Paginator ): JSX.Element => {
-  const [currentPortion, setCurrentPortion] = useState( Math.ceil( (currentItem + 1) / itemsPortionSize ) );
-  const pagesBar = [];
-  const totalCountPortion = Math.ceil( totalItemsCount / itemsPortionSize );
-  const leftNumberItemOfPortion = itemsPortionSize * (currentPortion - 1);
-  const rightNumberItemOfPortion = Math.min( itemsPortionSize * currentPortion, totalItemsCount ) - 1;
+const Paginator: React.FC<Paginator> = ({ totalItemsCount, currentItem, onClick, itemsPortionSize = 10 }) => {
+  const [currentPortion, setCurrentPortion] = useState(Math.ceil((currentItem + 1) / itemsPortionSize));
+  const pagesBar                            = [];
+  const totalCountPortion                   = Math.ceil(totalItemsCount / itemsPortionSize);
+  const leftNumberItemOfPortion             = itemsPortionSize * (currentPortion - 1);
+  const rightNumberItemOfPortion            = Math.min(itemsPortionSize * currentPortion, totalItemsCount) - 1;
 
   const handleClickPrev = () => {
-    setCurrentPortion( (prevState => prevState - 1) );
+    setCurrentPortion((prevState => prevState - 1));
   };
 
   const handleClickNext = () => {
-    setCurrentPortion( (prevState => prevState + 1) );
+    setCurrentPortion((prevState => prevState + 1));
   };
 
-  for ( let i = leftNumberItemOfPortion; i <= rightNumberItemOfPortion; i++ ) {
+  for (let i = leftNumberItemOfPortion; i <= rightNumberItemOfPortion; i++) {
     pagesBar.push(
       <li
-        key={ i }
-        className={ `${ classes.pages__item } ${ currentItem === i
-                                                 ? classes.pages__item_selected
-                                                 : undefined }` }
-        onClick={ () => onClick( i ) }>
-        { i + 1 }
+        key={i}
+        className={`${classes.pages__item} ${currentItem === i
+                                             ? classes.pages__item_selected
+                                             : undefined}`}
+        onClick={() => onClick(i)}>
+        {i + 1}
       </li>,
     );
   }
 
   return (
-    <div className={ classes.pages }>
-      { currentPortion > 1 && <button onClick={ handleClickPrev }>Prev</button> }
+    <div className={classes.pages}>
+      {currentPortion > 1 && <button onClick={handleClickPrev}>Prev</button>}
       <nav>
-        <ul className={ classes.pages__items }>
-          { pagesBar }
+        <ul className={classes.pages__items}>
+          {pagesBar}
         </ul>
       </nav>
-      { currentPortion < totalCountPortion && <button onClick={ handleClickNext }>Next</button> }
+      {currentPortion < totalCountPortion && <button onClick={handleClickNext}>Next</button>}
     </div>
   );
 };
