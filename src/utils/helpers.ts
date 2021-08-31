a2f218a657c888fd6488ae53806d90a3aea426d1
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-08-31 23:22:07 +0300

    type: fixed types

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,3 @@
-export const updateObjectInArray = ( arr: Array<any>, keyId: number, valueId: number, newProps: any ) => {
+export const updateObjectInArray = ( arr: Array<any>, keyId: string, valueId: string, newProps: any ) => {
   return arr.map( item => item[keyId] === valueId ? { ...item, ...newProps } : item );
 };
