0c4ae3ad603ccf6f9b77506ddfa41d09d0e21972
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-01 22:20:55 +0300

    type: fixed types in profile selector

--- src/redux/profile/selector.ts
+++ src/redux/profile/selector.ts
@@ -1,12 +1,5 @@
-export const getUserInfoState = ( state: any ) => {
-  return state.profilePage.userInfo;
-};
-export const getUserStatusState = ( state: any ) => {
-  return state.profilePage.userStatus;
-};
-export const getUserIdState = ( state: any ) => {
-  return state.profilePage.userId;
-};
-export const getPostsData = ( state: any ) => {
-  return state.profilePage.postsData;
-};
+import { AppStateType } from '../redux-store';
+
+export const getUserInfoState   = (state: AppStateType) => state.profilePage.userInfo;
+export const getUserStatusState = (state: AppStateType) => state.profilePage.userStatus;
+export const getPostsData       = (state: AppStateType) => state.profilePage.postsData;
