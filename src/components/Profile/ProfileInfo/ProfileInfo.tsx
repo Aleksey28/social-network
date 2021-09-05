4812c8a194220c0ea2b785f72ebe29a9f03d603e
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-05 16:55:37 +0300

    type: fixed types in ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -8,19 +8,26 @@ import { Profile } from '../../../types';
 
 interface Props {
   isOwner: boolean;
-  userInfo: Profile;
+  userInfo: Partial<Profile>;
   userStatus: string;
-  updateUserStatus: (status: string) => Promise<void>;
-  updateUserPhoto: (image: File) => Promise<void>;
-  updateUserData: (userData: Profile) => Promise<void>;
+  updateUserStatus: (status: string) => any;
+  updateUserPhoto: (image: File) => any;
+  updateUserData: (userData: Profile) => any;
 }
 
-const ProfileInfo: React.FC<Props> = ( { isOwner, userInfo, userStatus, updateUserStatus, updateUserPhoto, updateUserData } ) => {
+const ProfileInfo: React.FC<Props> = ({
+                                        isOwner,
+                                        userInfo,
+                                        userStatus,
+                                        updateUserStatus,
+                                        updateUserPhoto,
+                                        updateUserData
+                                      }) => {
   const { photos } = userInfo || {};
 
-  const handleChangePhoto = ( e: React.ChangeEvent<HTMLInputElement> ) => {
-    if ( e.target.files?.length ) {
-      updateUserPhoto( e.target.files[0] );
+  const handleChangePhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      updateUserPhoto(e.target.files[0]);
     }
   };
 
@@ -31,10 +38,10 @@ const ProfileInfo: React.FC<Props> = ( { isOwner, userInfo, userStatus, updateUs
         src="https://cdn.pixabay.com/photo/2020/12/19/03/27/person-5843476_960_720.jpg"
         alt="машина"/>
       <div>
-        <img className={ classes.info__avatar } src={ photos.large || emptyAvatar } alt="Avatar"/>
-        { isOwner && <input type="file" onChange={ handleChangePhoto }/> }
-        <ProfileData profileData={userInfo} updateUserData={ updateUserData }/>
-        <ProfileStatus status={ userStatus } updateUserStatus={ updateUserStatus }/>
+        <img className={classes.info__avatar} src={photos?.large || emptyAvatar} alt="Avatar"/>
+        {isOwner && <input type="file" onChange={handleChangePhoto}/>}
+        <ProfileData profileData={userInfo} updateUserData={updateUserData}/>
+        <ProfileStatus status={userStatus} updateUserStatus={updateUserStatus}/>
       </div>
     </div>
     : <Preloader/>
