7a9d7a107aace6d4f624f0375260caddbd25b0ad
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-05 17:07:46 +0300

    type: fixed in profile selector

--- src/redux/profile/selector.ts
+++ src/redux/profile/selector.ts
@@ -1,5 +1,6 @@
 import { AppStateType } from '../redux-store';
+import { Post, Profile } from '../../types';
 
-export const getUserInfoState   = (state: AppStateType) => state.profilePage.userInfo;
-export const getUserStatusState = (state: AppStateType) => state.profilePage.userStatus;
-export const getPostsData       = (state: AppStateType) => state.profilePage.postsData;
+export const getUserInfoState   = (state: AppStateType): Partial<Profile> => state.profilePage.userInfo || {};
+export const getUserStatusState = (state: AppStateType): string => state.profilePage.userStatus;
+export const getPostsData       = (state: AppStateType): Post[] => state.profilePage.postsData;
