617d76eb911728b4b4f80c1d4f308a2b73465027
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-05 17:00:49 +0300

    type: fixed types in DialogsContainer.tsx

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,27 +1,37 @@
-import { InitialState as DialogsInitialState, sendMessage } from '../../redux/dialogs/reducer';
+import { InitialState as DialogsInitialState, SendMessage, sendMessage } from '../../redux/dialogs/reducer';
 import Dialogs from './Dialogs';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { getDialogsPageState } from '../../redux/dialogs/selector';
 import { getFriendsState } from '../../redux/friends/selector';
 import { InitialState as FriendsInitialState } from '../../redux/friends/reducer';
+import { AppStateType } from '../../redux/redux-store';
 
-interface MapStateToProps {
+interface StateProps {
   dialogsPage: DialogsInitialState;
   friends: FriendsInitialState;
 }
 
-const mapStateToProps = (state: any): MapStateToProps => {
+interface DispatchProps {
+  sendMessage: (newMessage: string) => SendMessage;
+}
+
+interface OwnProps {
+}
+
+const mapStateToProps = (state: AppStateType): StateProps => {
   return {
     dialogsPage: getDialogsPageState(state),
     friends:     getFriendsState(state),
   };
 };
 
-const methods = {
+const methods: DispatchProps = {
   sendMessage,
 };
 
+const connector = connect<StateProps, DispatchProps, OwnProps, AppStateType>(mapStateToProps, methods);
+
 export default compose(
-  connect(mapStateToProps, methods),
+  connector,
 )(Dialogs);
