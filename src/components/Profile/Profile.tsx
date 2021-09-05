37f0c0e67ab0480d3431e2ae46f86fe326a10207
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-05 16:56:16 +0300

    type: fixed types in Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,23 +4,30 @@ import MyPostsContainer from './MyPosts/MyPostsContainer';
 import { Profile as ProfileInterface } from '../../types';
 
 interface Props {
-  userId: number;
-  userInfo: ProfileInterface;
+  userId: string | null;
+  userInfo: Partial<ProfileInterface>;
   userStatus: string;
-  updateUserStatus: (status: string) => Promise<void>;
-  updateUserPhoto: (image: File) => Promise<void>;
-  updateUserData: (userData: ProfileInterface) => Promise<void>;
+  updateUserStatus: (status: string) => any;
+  updateUserPhoto: (image: File) => any;
+  updateUserData: (userData: ProfileInterface) => any;
 }
 
-const Profile: React.FC<Props> = ( { userId, userInfo, userStatus, updateUserStatus, updateUserPhoto, updateUserData } ) => {
+const Profile: React.FC<Props> = ({
+                                    userId,
+                                    userInfo,
+                                    userStatus,
+                                    updateUserStatus,
+                                    updateUserPhoto,
+                                    updateUserData
+                                  }) => {
   return (
     <main>
-      <ProfileInfo isOwner={ userId === userInfo?.userId }
-                   userInfo={ userInfo }
-                   userStatus={ userStatus }
-                   updateUserStatus={ updateUserStatus }
-                   updateUserPhoto={ updateUserPhoto }
-                   updateUserData={ updateUserData }/>
+      <ProfileInfo isOwner={userId === userInfo?.userId}
+                   userInfo={userInfo}
+                   userStatus={userStatus}
+                   updateUserStatus={updateUserStatus}
+                   updateUserPhoto={updateUserPhoto}
+                   updateUserData={updateUserData}/>
       <MyPostsContainer/>
     </main>
   );
