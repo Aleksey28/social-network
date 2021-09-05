134caa1c443635b470ac788572d999679af51e33
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-05 15:41:37 +0300

    type: fixed types in Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,16 +3,16 @@ import ProfileInfo from './ProfileInfo/ProfileInfo';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import { Profile as ProfileInterface } from '../../types';
 
-interface ProfileProps {
+interface Props {
   userId: number;
   userInfo: ProfileInterface;
   userStatus: string;
-  updateUserStatus: any;
-  updateUserPhoto: any;
-  updateUserData: any;
+  updateUserStatus: (status: string) => Promise<void>;
+  updateUserPhoto: (image: File) => Promise<void>;
+  updateUserData: (userData: ProfileInterface) => Promise<void>;
 }
 
-const Profile = ( { userId, userInfo, userStatus, updateUserStatus, updateUserPhoto, updateUserData }: ProfileProps ): JSX.Element => {
+const Profile: React.FC<Props> = ( { userId, userInfo, userStatus, updateUserStatus, updateUserPhoto, updateUserData } ) => {
   return (
     <main>
       <ProfileInfo isOwner={ userId === userInfo?.userId }
