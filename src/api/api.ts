51a456070da1669a6b6ffe7259a8708e674a5ec7
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-08 23:20:41 +0300

    type: added type for ApiResponse

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { API_SAMURAI_SETTINGS } from '../utils/constants';
 
+export interface ApiResponse {
+  resultCode: number;
+  messages: string[];
+  data: {};
+}
+
 class API {
+  protected _instance: AxiosInstance;
+
   constructor() {
     this._instance = axios.create( {
       baseURL: API_SAMURAI_SETTINGS.baseUrl,
