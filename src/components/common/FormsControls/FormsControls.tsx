735ecefd2dcf04289124a3d77a4c265e1f472cdf
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-09-12 13:55:59 +0300

    refactor: reorganized FormsControls

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -2,34 +2,29 @@ import classes from './FormsControls.module.css';
 import React from 'react';
 import { WrappedFieldProps } from 'redux-form';
 
-interface Props {
-  Component: any;
-}
-
-const FromControl: React.FC<WrappedFieldProps & Props> = ({
-                                                            Component,
-                                                            input,
-                                                            meta: { valid, touched, error },
-                                                            ...   props
-                                                          }) => {
+const FromControl: React.FC<WrappedFieldProps> = ({ meta: { valid, touched, error }, children }) => {
   const hasError = !valid && touched;
   return (
     <div className={classes.control}>
-      <Component className={hasError ? classes.control__component : undefined} {...input} {...props}/>
+      {children}
       {hasError && <span className={classes.control__message}>{error}</span>}
     </div>
   );
 };
 
 export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+  const { input, meta, ...rest } = props;
+
   return (
-    <FromControl Component="textarea" {...props}/>
+    <FromControl {...props}><textarea {...input} {...rest}/></FromControl>
   );
 };
 
 export const Input: React.FC<WrappedFieldProps> = (props) => {
+  const { input, meta, ...rest } = props;
+
   return (
-    <FromControl Component="input" {...props}/>
+    <FromControl {...props}><input {...input} {...rest}/></FromControl>
   );
 };
 
