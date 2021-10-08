abc308c08ff7dd88cb7b76bd424ed1a9bce12818
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-10-08 11:41:38 +0300

    refactor: renamed type of props in HeaderContainer.tsx

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -6,36 +6,36 @@ import { logout, ThunkType } from '../../redux/auth/reducer';
 import { getEmailState, getIsAuthState, getLoginState, getUserIdState } from '../../redux/auth/selector';
 import { AppStateType } from '../../redux/redux-store';
 
-interface StateProps {
+interface StatePropsType {
   email: string;
   login: string;
   userId: string;
   isAuth: boolean;
 }
 
-interface DispatchProps {
+interface DispatchPropsType {
   logout: () => ThunkType;
 }
 
-interface OwnProps {
+interface OwnPropsType {
 }
 
-type Props = ConnectedProps<typeof connector>;
+type PropsType = ConnectedProps<typeof connector>;
 
-const mapStateToProps = (state: AppStateType): StateProps => ({
+const mapStateToProps = (state: AppStateType): StatePropsType => ({
   email:  getEmailState(state),
   login:  getLoginState(state),
   userId: getUserIdState(state),
   isAuth: getIsAuthState(state),
 });
 
-const mapDispatchToProps: DispatchProps = {
+const mapDispatchToProps: DispatchPropsType = {
   logout,
 };
 
-const connector = connect<StateProps, DispatchProps, OwnProps, AppStateType>(mapStateToProps, mapDispatchToProps);
+const connector = connect<StatePropsType, DispatchPropsType, OwnPropsType, AppStateType>(mapStateToProps, mapDispatchToProps);
 
-class HeaderContainer extends React.Component<Props> {
+class HeaderContainer extends React.Component<PropsType> {
   render () {
     return (
       <Header {...this.props}/>
