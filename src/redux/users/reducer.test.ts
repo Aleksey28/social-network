c36e18ee1b771ae95aa3c9725a8e02c31dd0e197
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-10-10 22:12:32 +0300

    test: added test Follow success

---
+++ src/redux/users/reducer.test.ts
@@ -0,0 +1,64 @@
+import reducer, { actions, InitialState } from './reducer';
+
+let state: InitialState;
+
+describe('Users reducer tests', () => {
+  beforeEach(() => {
+    state = {
+      users:                 [
+        {
+          id:       '1',
+          followed: false,
+          name:     'User 1',
+          photos:   {
+            large: '',
+            small: '',
+          },
+          status:   'Users 1 status'
+        },
+        {
+          id:       '2',
+          followed: false,
+          name:     'User 2',
+          photos:   {
+            large: '',
+            small: '',
+          },
+          status:   'Users 2 status'
+        },
+        {
+          id:       '3',
+          followed: true,
+          name:     'User 3',
+          photos:   {
+            large: '',
+            small: '',
+          },
+          status:   'Users 3 status'
+        },
+        {
+          id:       '4',
+          followed: true,
+          name:     'User 4',
+          photos:   {
+            large: '',
+            small: '',
+          },
+          status:   'Users 4 status'
+        },
+      ],
+      usersCount:            3,
+      pageSize:              5,
+      currentPage:           0,
+      isFetching:            false,
+      isTogglingFollowUsers: [],
+    };
+  });
+
+  test('Follow success', () => {
+    state = reducer(state, actions.setFollow('1'));
+
+    expect(state.users[0].followed).toBeTruthy();
+    expect(state.users[1].followed).toBeFalsy();
+  });
+});
