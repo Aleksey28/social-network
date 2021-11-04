64838f4b0a8c1447484aec6d4de05fcd27562bb0
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-04 17:39:11 +0400

    feat: added useSelector in the Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Preloader from '../common/Preloader/Preloader';
 import Paginator from '../common/Paginator/Paginator';
 import User from './User';
-import { InitialState as UsersInitialState, UserFiltersType } from '../../redux/users/reducer';
+import { UserFiltersType } from '../../redux/users/reducer';
 import SearchUsersForm from './SearchUsersForm';
+import { useSelector } from 'react-redux';
+import {
+  getCurrentPageState,
+  getFilters,
+  getIsFetchingState,
+  getIsTogglingFollowUsersState,
+  getPageSizeState,
+  getUsersCountState,
+  getUsersState,
+} from '../../redux/users/selector';
 
-interface Props extends UsersInitialState {
+interface Props {
   onPageChanged: (i: number) => void;
   onSearch: (filters: UserFiltersType) => void;
   follow: (id: string) => void;
@@ -14,18 +24,20 @@ interface Props extends UsersInitialState {
 
 const Users: React.FC<Props> = (props) => {
   const {
-          users,
-          usersCount,
-          pageSize,
-          currentPage,
-          isFetching,
-          isTogglingFollowUsers,
-          filters,
           onPageChanged,
           onSearch,
           follow,
           unfollow,
         } = props;
+
+  const users                 = useSelector(getUsersState);
+  const usersCount            = useSelector(getUsersCountState);
+  const currentPage           = useSelector(getCurrentPageState);
+  const pageSize              = useSelector(getPageSizeState);
+  const filters               = useSelector(getFilters);
+  const isTogglingFollowUsers = useSelector(getIsTogglingFollowUsersState);
+  const isFetching            = useSelector(getIsFetchingState);
+
 
   const countPages        = usersCount / pageSize;
   const handleClickOnPage = (i: number) => {
