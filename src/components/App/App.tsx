1d15d82b4968d375b8073ecb33b7dde87a6a2924
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-18 22:24:23 +0300

    style: added using antd in the App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
+import 'antd/dist/antd.css';
 import React, { lazy } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
-import SideBarContainer from '../SideBar/SideBarContainer';
 import ProfilePage from '../Profile/ProfilePage';
-import HeaderContainer from '../Header/HeaderContainer';
 import ProtectedRoute from '../../hoc/ProtectedRoute';
 import { compose } from 'redux';
 import { connect, ConnectedProps } from 'react-redux';
@@ -16,6 +15,12 @@ import { getIsAuthState } from '../../redux/auth/selector';
 import withSuspense from '../../hoc/withSuspense';
 import { AppStateType } from '../../redux/redux-store';
 import LoginPage from '../Login/LoginPage';
+
+import { Breadcrumb, Layout } from 'antd';
+import Logo from '../Logo/Logo';
+import Header from '../Header/Header';
+
+const { Content, Footer, Sider } = Layout;
 
 const DialogsContainer = lazy(() => import('../Dialogs/DialogsContainer').then(DialogsContainer => DialogsContainer));
 const UsersPage        = lazy(() => import('../Users/UsersPage').then(UsersPage => UsersPage));
@@ -49,44 +54,67 @@ const Dialogs = withSuspense(DialogsContainer);
 const Users   = withSuspense(UsersPage);
 
 class App extends React.Component<PropsType> {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = (collapsed: boolean) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   componentDidMount () {
     this.props.initializing();
   }
 
   render () {
     let { isAuth, initialized } = this.props;
+    const { collapsed }         = this.state;
+
     return initialized
            ? (
-             <div className="app-wrapper">
-               <HeaderContainer/>
-               <Navbar/>
-               <SideBarContainer/>
-               <div className="app-wrapper__content">
-                 <Switch>
-                   <Redirect exact from="/" to="/profile"/>
-                   <Route path="/login">
-                     <LoginPage/>
-                   </Route>
-                   <Route path="/profile/:userId?">
-                     <ProfilePage/>
-                   </Route>
-                   <ProtectedRoute path="/messages"
-                                   condition={isAuth}
-                                   to={'/login'}
-                                   render={() => <Dialogs/>}/>
-                   <ProtectedRoute path="/users"
-                                   condition={isAuth}
-                                   to={'/login'}
-                                   render={() => <Users/>}/>
-                   <Route path="/error">
-                     <div>
-                       ERROR 404 =D
-                     </div>
-                   </Route>
-                   <Redirect to="/error"/>
-                 </Switch>
-               </div>
-             </div>
+             <Layout style={{ minHeight: '100vh' }}>
+               <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+                 <Logo/>
+                 <Navbar/>
+               </Sider>
+               <Layout className="site-layout">
+                 <Header/>
+                 <Content style={{ margin: '0 16px' }}>
+                   <Breadcrumb style={{ margin: '16px 0' }}>
+                     <Breadcrumb.Item>User</Breadcrumb.Item>
+                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                   </Breadcrumb>
+                   <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                     <Switch>
+                       <Redirect exact from="/" to="/profile"/>
+                       <Route path="/login">
+                         <LoginPage/>
+                       </Route>
+                       <Route path="/profile/:userId?">
+                         <ProfilePage/>
+                       </Route>
+                       <ProtectedRoute path="/messages"
+                                       condition={isAuth}
+                                       to={'/login'}
+                                       render={() => <Dialogs/>}/>
+                       <ProtectedRoute path="/users"
+                                       condition={isAuth}
+                                       to={'/login'}
+                                       render={() => <Users/>}/>
+                       <Route path="/error">
+                         <div>
+                           ERROR 404 =D
+                         </div>
+                       </Route>
+                       <Redirect to="/error"/>
+                     </Switch>
+                   </div>
+                 </Content>
+                 <Footer
+                   style={{ textAlign: 'center' }}>{`ForRest©${new Date().getFullYear()} Created by Aleksey Popov`}</Footer>
+               </Layout>
+             </Layout>
            )
            : <Preloader/>;
   }
