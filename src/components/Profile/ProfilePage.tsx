a27acfe9db6b1327910111e92825030ae36e71b8
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-30 22:14:36 +0300

    refactor: fixed warnings in the ProfilePage.tsx

--- src/components/Profile/ProfilePage.tsx
+++ src/components/Profile/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserInfo, getUserStatus, } from '../../redux/profile/reducer';
 import { useHistory, useParams } from 'react-router';
@@ -10,26 +10,27 @@ const ProfilePage: React.FC = () => {
   const history  = useHistory();
   const dispatch = useDispatch();
   const params   = useParams<{ userId: string }>();
-  let userId     = useSelector(getUserIdState);
+  const userId     = useSelector(getUserIdState);
+  const refUserId  = useRef(userId);
 
-  const refreshProfileInfo = () => {
-    userId = params.userId || userId || '';
+  const refreshProfileInfo = useCallback(() => {
+    refUserId.current = params.userId || refUserId.current || '';
 
-    if (!userId) {
+    if (!refUserId.current) {
       history.push('/login');
     }
 
-    dispatch(getUserInfo(userId));
-    dispatch(getUserStatus(userId));
-  };
+    dispatch(getUserInfo(refUserId.current));
+    dispatch(getUserStatus(refUserId.current));
+  }, [params, dispatch, history]);
 
   useEffect(() => {
     refreshProfileInfo();
-  }, []);
+  }, [refreshProfileInfo]);
 
   useEffect(() => {
     refreshProfileInfo();
-  }, [params]);
+  }, [params, refreshProfileInfo]);
 
   return (
     <main>
