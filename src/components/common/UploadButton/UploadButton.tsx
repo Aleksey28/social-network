ef18a9221e0f16731d854c36b56c0e9a9660b1c9
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-01-09 23:01:59 +0300

    fix: fixed uploading avatar

--- src/components/common/UploadButton/UploadButton.tsx
+++ src/components/common/UploadButton/UploadButton.tsx
@@ -8,11 +8,15 @@ interface UploadButtonProps {
 }
 
 const UploadButton: React.FC<UploadButtonProps> = ({ onChange, ...props }) => {
-  const input = useRef(null);
+  const input = useRef<HTMLInputElement>(null);
+
+  const handleClickButton = () => {
+    input.current?.click();
+  };
 
   return <div>
     <input ref={input} type="file" onChange={onChange} className={classes.input}/>
-    <Button className={classes.button}>
+    <Button className={classes.button} onClick={handleClickButton}>
       <UploadOutlined/> Click to Upload
     </Button>
   </div>;
