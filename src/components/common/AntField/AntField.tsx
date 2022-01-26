d3aa3dab4ec26563bde41565524f3a680d07f9b3
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-01-26 20:27:25 +0300

    feat: added AntCheckbox

--- src/components/common/AntField/AntField.tsx
+++ src/components/common/AntField/AntField.tsx
@@ -1,10 +1,11 @@
-import { Form, Input } from 'antd';
+import { Checkbox, Form, Input } from 'antd';
 import React from 'react';
 import { FieldProps } from 'formik';
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 const { TextArea } = Input;
 
-type AntComponentsType = typeof TextArea | typeof Input;
+type AntComponentsType = typeof TextArea | typeof Input | typeof Checkbox;
 type PropsType = FieldProps & { submitCount: number, hasFeedback: boolean }
 
 const AntFieldCreator = (AntComponent: AntComponentsType) => ({
@@ -21,13 +22,14 @@ const AntFieldCreator = (AntComponent: AntComponentsType) => ({
   const submittedError = hasError && submitted;
   const touchedError   = hasError && touched;
   const handleBlur     = () => form.setFieldTouched(field.name, true);
-  const handleChange   = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => form.setFieldValue(field.name, e.target.value);
+  const handleChange   = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | CheckboxChangeEvent) => form.setFieldValue(field.name, e.target.value);
 
   return (
     <Form.Item
       hasFeedback={!!(hasFeedback && (submitted || touched))}
       help={submittedError || touchedError ? hasError : false}
       validateStatus={submittedError || touchedError ? 'error' : 'success'}
+      {...props}
     >
       <AntComponent
         {...field}
@@ -41,3 +43,4 @@ const AntFieldCreator = (AntComponent: AntComponentsType) => ({
 
 export const AntTextArea = AntFieldCreator(TextArea);
 export const AntInput    = AntFieldCreator(Input);
+export const AntCheckbox = AntFieldCreator(Checkbox);
