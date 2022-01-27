2066d41ac42202c59b18e2a3f95b0086e93092ad
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-01-27 20:29:24 +0300

    feat: added ProfileDataMainForm

---
+++ src/components/Profile/ProfileInfo/ProfileData/ProfileMainData/ProfileDataMainForm.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ProfileType } from '../../../../../types';
+import { Field, Formik } from 'formik';
+import { FormikHelpers } from 'formik/dist/types';
+import { Button, Form } from 'antd';
+import { required } from '../../../../../utils/validators';
+import { AntCheckbox, AntInput } from '../../../../common/AntField/AntField';
+
+interface PropsType {
+  onSubmit: (profileInfo: Partial<ProfileType>) => void;
+  initialValues: Partial<ProfileType>;
+  onCancel: () => void;
+}
+
+const ProfileDataMainForm: React.FC<PropsType> = ({ onSubmit, onCancel, initialValues }) => {
+
+  const handleSubmit = async (values: Partial<ProfileType>, { setSubmitting }: FormikHelpers<Partial<ProfileType>>) => {
+    await onSubmit(values);
+    setSubmitting(false);
+  };
+
+  return (
+    <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+      {({ isSubmitting, submitForm }) => (
+        <Form onFinish={submitForm} size="small">
+          <Field component={AntInput} name="fullName" validate={required} label="Name" allowClear hasFeedback/>
+          <Field component={AntInput} name="aboutMe" validate={required} label="About me" allowClear hasFeedback/>
+          <Field component={AntCheckbox} name="lookingForAJob" label="Looking for a job" allowClear
+                 hasFeedback/>
+          <Field component={AntInput} name="lookingForAJobDescription" validate={required} label="Skills" allowClear
+                 hasFeedback/>
+          <Button type="primary" htmlType="submit" disabled={isSubmitting}>Save</Button>
+          <Button disabled={isSubmitting} onClick={onCancel}>Cancel</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ProfileDataMainForm;
