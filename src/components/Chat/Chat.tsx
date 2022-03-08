c1705252b097a544daeaf3f890860a4b82e6a9b1
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-03-08 19:40:08 +0400

    feat: added chatWS

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,6 +3,13 @@ import Messages from "./Messages/Messages";
 import NewMessage from "./NewMessage/NewMessage";
 
 const Chat: React.FC = () => {
+  const [chatWS, setChatWS] = useState<WebSocket>()
+
+  useEffect(() => {
+    const chatWS = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx');
+
+    setChatWS(chatWS)
+  }, [])
   return (
     <div>
       <Messages />
