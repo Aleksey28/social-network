7bc32014fe12c0fa42c0854438ea76d06ba642be
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-03-09 14:41:40 +0400

    feat: read messages from chatWS

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Messages from "./Messages/Messages";
 import NewMessage from "./NewMessage/NewMessage";
 
+export interface MessageProps {
+  message: string;
+  photo: string;
+  userId: number;
+  userName: string;
+}
+
 const Chat: React.FC = () => {
+
+  const [messages, setMessages] = useState<MessageProps[]>([]);
   const [chatWS, setChatWS] = useState<WebSocket>()
 
   useEffect(() => {
     const chatWS = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx');
 
     setChatWS(chatWS)
+
+    chatWS.addEventListener('message', (e) => {
+      setMessages((prev) => [...prev, ...JSON.parse(e.data)])
+    })
   }, [])
+
+
   return (
     <div>
       <Messages />
